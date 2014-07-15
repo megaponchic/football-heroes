5736b772add23b784dba52692679de161c7e0d44
Artem Panchoyan <artem.panchoyan@gmail.com>
2014-07-15 22:32:33 +0200

    finished version 0.0.1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 /**
- * @fileOverview Main js file of web application that showed some football statistics in a nice way
+ * @fileOverview Main js file of web application that showes some football statistics in a nice way
  * @author Artem Panchoyan
  * @copyright Artem Panchoyan
  * @license http://opensource.org/licenses/MIT MIT License
  * @version 0.0.1
  * 
  */
-'use strict';
+(function ($) {
 
-(function($) {
+    'use strict';
 
     /**
      * Some default properties
+     * 
      * @type {Object}
      */
     var defaults = {
 
         /**
          * Base URL to fetch football.db data
+         * 
          * @type {String}
          */
         baseUrl: 'http://footballdb.herokuapp.com/api/v1'
-    }
+    };
 
     /**
      * @namespace API to interact with football.db service 
@@ -31,6 +33,7 @@
 
         /**
          * Get all teams (countries with description) that qualified in the contest
+         * 
          * @param  {String} key Contest key
          * @example getting world cup 2014 teams
          * var wc2014teams = [];
@@ -41,17 +44,21 @@
          * @return {jqXHR} Promise-like object that will resolve with teams for the contest
          * https://github.com/openfootball/api/blob/master/TEAMS.md
          */
-        getTeams : function(key) {
+        getTeams : function (key) {
             var promise;
-            return promise = $.ajax({
+
+            promise = $.ajax({
                 url: defaults.baseUrl + '/event/' + key + '/teams/',
                 crossDomain: true,
                 dataType: 'jsonp'
             });
+
+            return promise;
         },
 
         /**
          * List all games in a round for an event
+         * 
          * @param  {String} key   Contest key
          * @param  {Number} round Round number
          * @return {jqXHR} Promise-like object that will resolve with teams for the contest
@@ -62,30 +69,105 @@
          * });
          * @example
          */
-        getRound : function(key, round) {
+        getRound : function (key, round) {
             var promise;
-            return promise = $.ajax({
+
+            promise = $.ajax({
                 url: defaults.baseUrl + '/event/' + key + '/round/' + round,
                 crossDomain: true,
                 dataType: 'jsonp'
-            });            
+            });
+
+            return promise;
         }
     };
 
-    $(document).ready(function() {
+    /**
+     * once dom is loaded the main app launches and shows results of world cup 2014
+     * @private
+     */
+    $(document).ready(function () {
 
-        var wc2014teams = [];
-        var teamsPromise;
+        var wc2014rounds = [];
+        var wc2014playOffRounds = [];
+        var roundsPromises = [];
+        var worldCupRoundsNumber = 20;
         
-        teamsPromise = API.getTeams('world.2014').then(function(data) {
-            wc2014teams = data.teams;
-        }, function(jqXHR, textStatus, errorThrown) {
-            // TODO: implement failed request
-        })
+        // each round info is a separate call to web-service
+        // wrapping all in promises and will work after all resolved
+        for (var i = worldCupRoundsNumber; i >= 1; i--) {
+            var roundPromise = API.getRound('world.2014', i).then(function (data) {
+                wc2014rounds.push(data);
+            }, function (jqXHR, textStatus, errorThrown) {
+                // TODO: implement failed request
+            });
+            roundsPromises.push(roundPromise);
+        }
 
-        teamsPromise.then(function() {
-            $('.teams').html('Fetched');
+        // when all rounds info fetched
+        $.when.apply($, roundsPromises).then(function() {
+            $('.loader').hide();
+
+            // get only play-offs and assign the divs to each round
+            wc2014playOffRounds = wc2014rounds.filter(function(el) {
+                if (el.round.pos === 16) {
+                    el.round.divs = [1,7];
+                    return true;
+                } else if (el.round.pos === 17) {
+                    el.round.divs = [2,6];
+                    return true;
+                } else if (el.round.pos === 18) {
+                    el.round.divs = [3,5];
+                    return true;
+                } else if (el.round.pos === 20) {
+                    el.round.divs = [4,4];
+                    return true;
+                }
+            });
+
+            // iterate over rounds and show results
+            for (var i = wc2014playOffRounds.length - 1; i >= 0; i--) {
+                for (var j = wc2014playOffRounds[i].games.length - 1; j >= 0; j--) {
+                    
+                    var scoreString1,
+                        scoreString2;
+
+                    // victory can be with penalty kicks so score results are complex
+                    if (wc2014playOffRounds[i].games[j].score1ot) {
+                        scoreString1 = wc2014playOffRounds[i].games[j].score1ot;
+                        scoreString2 = wc2014playOffRounds[i].games[j].score2ot;
+
+                        if (wc2014playOffRounds[i].games[j].score1p) {
+                            scoreString1 += '&nbsp;' + '(' + wc2014playOffRounds[i].games[j].score1p + ')';
+                            scoreString2 += '&nbsp;' + '(' + wc2014playOffRounds[i].games[j].score2p + ')';
+                        }
+                    } else {
+                        scoreString1 = wc2014playOffRounds[i].games[j].score1;
+                        scoreString2 = wc2014playOffRounds[i].games[j].score2;
+
+                    }
+
+                    var roundTemplate = '<div class="round-info-wrapper"><div class="round-info">' + 
+                            wc2014playOffRounds[i].games[j].team1_code + '&nbsp;&ndash;&nbsp;' + 
+                            wc2014playOffRounds[i].games[j].team2_code + 
+                            '</div>' + 
+                            '<div class="round-info">' + 
+                            scoreString1 + '&nbsp;&ndash;&nbsp;' + scoreString2 + 
+                            '</div></div>';
+
+                    if (j < wc2014playOffRounds[i].games.length/2) {
+                        $('.round:nth-child(' + wc2014playOffRounds[i].round.divs[0] + ')')
+                            .append(roundTemplate);
+                    } else {
+                        $('.round:nth-child(' + wc2014playOffRounds[i].round.divs[1] + ')')
+                            .append(roundTemplate);
+                    }
+                    
+                };
+            };
+        }, function(e) {
+            console.log("Failed to fetch rounds");
         });
-    })
+    });
     
 })(jQuery);
